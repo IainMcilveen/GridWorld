d748e104db5bc5990398cf3f68f4e365994ee058
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2019-08-23 12:42:47 -0300

    changes

--- src/com/Iain/gridworld/Entity.java
+++ src/com/Iain/gridworld/Entity.java
@@ -5,5 +5,13 @@ public class Entity {
 	private int y;
 	private int trueX;
 	private int trueY;
+	private int textureWidth;
+	private int textureHeight;
+	
+	public Entity(int x, int y, int textWid, int textH) {
+		this.x = x;
+		this.y = y;
+		
+	}
 
 }
