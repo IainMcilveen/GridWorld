2601c5c6f073e21f94883c761274eccff274a7d5
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2019-08-23 13:04:07 -0300

    added a player class

---
+++ src/com/Iain/gridworld/Player.java
@@ -0,0 +1,11 @@
+package com.Iain.gridworld;
+
+import java.awt.image.BufferedImage;
+
+public class Player extends Entity {
+	
+	public Player(int x, int y, int textWid, int textH, BufferedImage texture) {
+		super(x,y,textWid,textH,texture);
+	}
+	
+}
