c72a61b365f2185b4720e61df3bad57acc952a38
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2019-08-23 12:46:13 -0300

    added base entity class

--- src/com/Iain/gridworld/Entity.java
+++ src/com/Iain/gridworld/Entity.java
@@ -1,5 +1,7 @@
 package com.Iain.gridworld;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
 	private int x;
 	private int y;
@@ -7,10 +9,16 @@ public class Entity {
 	private int trueY;
 	private int textureWidth;
 	private int textureHeight;
+	private BufferedImage entityTexture;
 	
-	public Entity(int x, int y, int textWid, int textH) {
+	public Entity(int x, int y, int textWid, int textH, BufferedImage texture) {
 		this.x = x;
 		this.y = y;
+		this.textureWidth = textWid;
+		this.textureHeight = textH;
+		this.trueX = x * trueX;
+		this.trueY = y * trueY;
+		this.entityTexture = texture;
 		
 	}
 
